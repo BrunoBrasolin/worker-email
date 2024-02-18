5a9ac6d23c8a4828282434156c502b17746365d6
Bruno Brasolin <brunobrasolinc@gmail.com>
2024-02-18 03:43:06 -0300

    Change appsettings on prod

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 using Gamidas.Utils;
 using worker_email;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.ConfigureGamidas();
-        services.AddHostedService<WorkerEmail>();
-    })
-    .Build();
+HostBuilder builder = new();
+builder.ConfigureAppConfiguration((hostingContext, config) =>
+{
+	config.SetBasePath("/app/config");
+	config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+});
+
+IHost host = builder.ConfigureServices(args)
+	.ConfigureServices(services =>
+	{
+		services.ConfigureGamidas();
+		services.AddHostedService<WorkerEmail>();
+	})
+	.Build();
 
 host.Run();
